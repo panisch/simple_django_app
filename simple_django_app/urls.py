32f8aa68ca2931300da5dde9ca7e5f4feb84442a
Patrick Nisch <patrick.nisch@googlemail.com>
2012-09-27 21:15:50 +0200

    allow blitz.io to test

--- simple_django_app/urls.py
+++ simple_django_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^mu-4b9a3550-85944871-f8b6aaf5-0ea7cfde$", direct_to_template, {"template": "42.html"}),
 )
