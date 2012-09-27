cd9b4cc61bd5fa13b512617bd557fc65058bc636
Patrick Nisch <patrick.nisch@googlemail.com>
2012-09-27 17:15:54 +0200

    initial commit

---
+++ test_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
